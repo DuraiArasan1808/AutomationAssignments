9505a767479d4f17e0dae73af5c7bba698f11ce6
DuraiArasan1808 <duraiarasanrajagopal@20cube.com>
2024-12-23 21:51:40 +0530

    Assignment 2 - Arrays

---
+++ src/com/creatio/crm/language/basics/ArrayAssignment2.java
@@ -0,0 +1,97 @@
+package com.creatio.crm.language.basics;
+
+/**
+ * A supermarket offers products in two main categories: "Groceries" and "Electronics." Each category is further divided  into four 
+ * subcategories, with each subcategory containing five products.
+ * 
+ * Can you store complete data in single Array? 
+ * 
+ */
+
+public class ArrayAssignment2 {
+
+	public static void main(String[] args) {
+		
+		String[] superMarket = {
+				// Main Category: Groceries
+				"Groceries",
+				"Beverages", "Cola","Orange Juice", "Lemonade", "Green Tea","Black Coffee",
+				"Snacks","Popcorn", "Trail Mix", "Beef Jerky", "Salted Nuts", "Rice Cakes",
+				"Dairy Products", "Cottage Cheese", "Heavy Cream", "Ice Cream", "Parmesan Cheese", "Eggnog",
+				"Produce", "Apples", "Bananas", "Carrots", "Potatoes", "Onions",
+				
+				// Main Category: Electronics
+				"Electroncis",
+				"Beverages", "Cola","Orange Juice", "Lemonade", "Green Tea","Black Coffee",
+				"Snacks","Popcorn", "Trail Mix", "Beef Jerky", "Salted Nuts", "Rice Cakes",
+				"Dairy Products", "Cottage Cheese", "Heavy Cream", "Ice Cream", "Parmesan Cheese", "Eggnog",
+				"Produce", "Apples", "Bananas", "Carrots", "Potatoes", "Onions"
+		};
+				
+				// Display Groceries Data
+				System.out.println("Main Category : " +  superMarket[0]);
+				System.out.println("Sub Category : " + superMarket[1]);
+				System.out.println("Product 1 : " + superMarket[2]);
+				System.out.println("Product 2 : " + superMarket[3]);
+				System.out.println("Product 3 : " + superMarket[4]);
+				System.out.println("Product 4 : " + superMarket[5]);
+				System.out.println("Product 5 : " + superMarket[6]);
+				
+				System.out.println("Sub Category : " + superMarket[7]);
+				System.out.println("Product 1 : " + superMarket[8]);
+				System.out.println("Product 2 : " + superMarket[9]);
+				System.out.println("Product 3 : " + superMarket[10]);
+				System.out.println("Product 4 : " + superMarket[11]);
+				System.out.println("Product 5 : " + superMarket[12]);
+				
+				System.out.println("Sub Category : " + superMarket[13]);
+				System.out.println("Product 1 : " + superMarket[14]);
+				System.out.println("Product 2 : " + superMarket[15]);
+				System.out.println("Product 3 : " + superMarket[16]);
+				System.out.println("Product 4 : " + superMarket[17]);
+				System.out.println("Product 5 : " + superMarket[18]);
+				
+				System.out.println("Sub Category : " + superMarket[19]);
+				System.out.println("Product 1 : " + superMarket[20]);
+				System.out.println("Product 2 : " + superMarket[21]);
+				System.out.println("Product 3 : " + superMarket[22]);
+				System.out.println("Product 4 : " + superMarket[23]);
+				System.out.println("Product 5 : " + superMarket[24]);
+				
+				//Display Electronics Data
+				System.out.println("Main Category : " +  superMarket[25]);
+				System.out.println("Sub Category : " + superMarket[26]);
+				System.out.println("Product 1 : " + superMarket[27]);
+				System.out.println("Product 2 : " + superMarket[28]);
+				System.out.println("Product 3 : " + superMarket[29]);
+				System.out.println("Product 4 : " + superMarket[30]);
+				System.out.println("Product 5 : " + superMarket[31]);
+				
+				System.out.println("Sub Category : " + superMarket[32]);
+				System.out.println("Product 1 : " + superMarket[33]);
+				System.out.println("Product 2 : " + superMarket[34]);
+				System.out.println("Product 3 : " + superMarket[35]);
+				System.out.println("Product 4 : " + superMarket[36]);
+				System.out.println("Product 5 : " + superMarket[37]);
+				
+				System.out.println("Sub Category : " + superMarket[38]);
+				System.out.println("Product 1 : " + superMarket[39]);
+				System.out.println("Product 2 : " + superMarket[40]);
+				System.out.println("Product 3 : " + superMarket[41]);
+				System.out.println("Product 4 : " + superMarket[42]);
+				System.out.println("Product 5 : " + superMarket[43]);
+				
+				System.out.println("Sub Category : " + superMarket[44]);
+				System.out.println("Product 1 : " + superMarket[45]);
+				System.out.println("Product 2 : " + superMarket[46]);
+				System.out.println("Product 3 : " + superMarket[47]);
+				System.out.println("Product 4 : " + superMarket[48]);
+				System.out.println("Product 5 : " + superMarket[49]);
+				
+		
+		
+		
+
+	}
+
+}
---
+++ src/com/creatio/crm/language/basics/DataTypesAssignment1.java
@@ -0,0 +1,97 @@
+package com.creatio.crm.language.basics;
+
+/**
+ * Assignment1-19-12-2024 - Data Types
+ * This program explains the use of various data types in JAVA
+ *
+*/
+
+public class DataTypesAssignment1 {
+
+	public static void main(String[] args) {
+		// 1. Temperature of a city in degrees Celsius: 25.5
+		double cityTemp = 25.5; // double is used for Decimal values
+		System.out.println("City Temperature : " + cityTemp + "Celsius");
+		
+		// 2. Whether a customer has placed an order: true or false
+		boolean hasPlacedOrder = true; // boolean is used for true / false value
+		System.out.println("Customer has placed an order : " + hasPlacedOrder);
+		
+		// 3. Person's phone number: "123-456-7890"
+		String phoneNumber = "123-456-7890"; // String is used for text data
+	    System.out.println("Person's Phone Number : " + phoneNumber);
+		
+	    // 4. Amount of money in a customer's bank account: 1000.50
+	    double accountBalance = 1000.50; //double is used for currency values
+	    System.out.println("Account Balance : " + accountBalance);
+	    
+	    // 5. Person's email address: "john.doe@example.com"
+	    String emailAddress = "john.doe@2xample.com"; // String is s+used for text data
+	    System.out.println("Email Address : " + emailAddress);
+	    
+	    // 6. Coordinates of a location (latitude, longitude): 37.7749, -122.4194
+	    double lattitude = 37.7749; // decimal is used for precise decimal
+	    double longitude = -122.4194;  // decimal is used for precise decimal
+	    System.out.println("Location Coordinates : " + lattitude + "&" + longitude);
+	    
+	    // 7. Person's marital status: true or false
+	    boolean maritalStatus = true;
+	    System.out.println("Marital Status : " + maritalStatus);
+	    
+	    // 8. Person's occupation: "Software Engineer"
+	    String personOccupation = "Software Engineer"; // String is s+used for text data
+	    System.out.println("Person's Occupation : " + personOccupation);
+	    
+	    // 9. Person's Favorite color: "Blue"
+	    String favouriteColor = "Blue"; // String is s+used for text data
+	    System.out.println("Person's Favourite Colour : " +favouriteColor);
+	    
+	    // 10.Current year: 2023
+	    int currentYear = 2023;
+	    System.out.println("Current Year : " + currentYear);
+	    
+	    // 11.Number of followers on a social media platform: 1,000,000
+	    String numberOfFollowers = "1,000,000"; // String is s+used for text data
+	    System.out.println("Number Of Followers on a social media platform:" + numberOfFollowers);
+	    
+	    // 12.Rating of a movie: 7.5
+	    float ratingOfMovie = 7.5f; 
+	    System.out.println("Rating Of a Movie : " + ratingOfMovie);
+	    
+	    // 13.Person's blood type: 'A'
+	    char bloodType = 'A'; //char is used for single character, it should be in single quotes
+	    System.out.println("Blood Type of the Person : " + bloodType);
+	    
+	    // 14.Title of a book: "To Kill a Mockingbird"
+	    String titleOfBook = "To Kill a Mockingbird"; // String is s+used for text data
+	    System.out.println("Title of a Book : " + titleOfBook);
+	    
+	    // 15.Number of employees in a company: 500
+	    int numberOfEmployees = 500; // int is used for whole numbers
+	    System.out.println("Number Of Employees in a company : " + numberOfEmployees);
+	    
+	    // 16.Time of an event: 2:30 PM
+	    String timeOfEvent = "2:30 PM"; // String is s+used for text data
+	    System.out.println("Time of an event : " + timeOfEvent);
+	    
+	    // 17.Name of a country: "United States"
+	    String nameOfCountry = "United States"; // String is s+used for text data
+	    System.out.println("Name of a country : " + nameOfCountry);
+	    
+	    // 18.Person's eye color: "Brown"
+	    String eyeColour = "Brown"; // String is s+used for text data
+	    System.out.println("Person's eye color : " + eyeColour);
+	    
+	    // 19.Person's birthplace: "New York City"
+	    String birthPlace = "New York City"; // String is s+used for text data
+	    System.out.println("Person's Birth Place : " + birthPlace);
+	    
+	    // 20. Distance between two cities: 200.5
+	    double distanceBtw2Cities = 200.5; //double is used for currency value
+	    System.out.println("Distance between two cities : " + distanceBtw2Cities);
+		
+		
+
+	}
+
+}
